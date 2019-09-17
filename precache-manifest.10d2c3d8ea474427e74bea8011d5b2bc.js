5a0d4e973bfd130648c168f90905f2e33223df78
Vijayakumar <vmathiyalagan@concertcare.com>
2019-09-17 11:41:34 +0530

    Updates

---
+++ precache-manifest.10d2c3d8ea474427e74bea8011d5b2bc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41fde8530ffabbe232faaea5caaacaf",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "2f9eb36cef1f51b9c57d",
+    "url": "/ecommerce/static/css/main.12c86133.chunk.css"
+  },
+  {
+    "revision": "b162a6b473ecd47e6a1f",
+    "url": "/ecommerce/static/js/2.bf44f864.chunk.js"
+  },
+  {
+    "revision": "2f9eb36cef1f51b9c57d",
+    "url": "/ecommerce/static/js/main.75b8cecb.chunk.js"
+  },
+  {
+    "revision": "4b62c7e58817c980e513",
+    "url": "/ecommerce/static/js/runtime~main.fed20531.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/ecommerce/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2781bf543c08d394dc78dec86989e3fd",
+    "url": "/ecommerce/static/media/Linearicons-Free.2781bf54.svg"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/ecommerce/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/ecommerce/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/ecommerce/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "c425fda07225495ca25b3b89fcf92b00",
+    "url": "/ecommerce/static/media/common-banner.c425fda0.jpg"
+  },
+  {
+    "revision": "7267cfdcf44b69d7a8ba1c048bf77725",
+    "url": "/ecommerce/static/media/exclusive.7267cfdc.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ecommerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ecommerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ecommerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ecommerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ecommerce/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5c4ac3841816680eec34b5f4054b4b09",
+    "url": "/ecommerce/static/media/p1.5c4ac384.jpg"
+  },
+  {
+    "revision": "acdf92f2912e89fedf0d90e94bcc60ab",
+    "url": "/ecommerce/static/media/p12.acdf92f2.jpg"
+  },
+  {
+    "revision": "2e64bfe23477a018a66620aaf49701b1",
+    "url": "/ecommerce/static/media/p2.2e64bfe2.jpg"
+  },
+  {
+    "revision": "0441fa90c7c55896839aab1b44254029",
+    "url": "/ecommerce/static/media/p3.0441fa90.jpg"
+  },
+  {
+    "revision": "755626c9317da71b6ef2b2d199995d68",
+    "url": "/ecommerce/static/media/p4.755626c9.jpg"
+  },
+  {
+    "revision": "b1dbbe081839426e7dad35ea95074d4d",
+    "url": "/ecommerce/static/media/p5.b1dbbe08.jpg"
+  },
+  {
+    "revision": "ba2b72abedb5debce12b7be40e1b58c7",
+    "url": "/ecommerce/static/media/p6.ba2b72ab.jpg"
+  },
+  {
+    "revision": "a5487a03160dd95b708ee30bed4719cf",
+    "url": "/ecommerce/static/media/p7.a5487a03.jpg"
+  },
+  {
+    "revision": "7bde9956930b6f5585903556f2cb3c00",
+    "url": "/ecommerce/static/media/p8.7bde9956.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/ecommerce/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/ecommerce/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/ecommerce/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/ecommerce/static/media/themify.e23a7dca.ttf"
+  }
+]);
